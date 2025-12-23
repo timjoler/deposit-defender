3968c3e7bcbf0be3fab4878f118f607ea2518afa
Tim Joler <timjoler@yahoo.co.uk>
2025-12-23 21:40:24 +0000

    Fix stripe version

--- app/api/create-checkout/route.ts
+++ app/api/create-checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  typescript: true,
+  typescript: true, apiVersion: '2025-12-15.clover',
 });
 
 export async function POST(req: Request) {
--- app/api/verify-payment/route.ts
+++ app/api/verify-payment/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  typescript: true,
+  apiVersion: '2025-12-15.clover',
 });
 
 export async function POST(req: Request) {
